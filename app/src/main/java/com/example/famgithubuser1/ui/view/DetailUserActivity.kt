b10ff20f742e075b8365e1f647caf0188f951f6b
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-06-06 00:30:07 +0700

    update: add detail user activity

---
+++ app/src/main/java/com/example/famgithubuser1/ui/view/DetailUserActivity.kt
@@ -0,0 +1,12 @@
+package com.example.famgithubuser1.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.famgithubuser1.R
+
+class DetailUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_user)
+    }
+}
